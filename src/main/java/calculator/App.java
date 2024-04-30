23264ee269bc9ee99094827cd66eadfad86cfb26
jiwoo <1114jiwoo@gmail.com>
2024-04-30 16:39:52 +0900

    Level1 - 3

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -16,7 +16,24 @@ public class App {
         /* charAt(idx) : charAt 메서드는 매개변수로 char 타입으로 변환하고자하는 문자열 위치 받는다*/
         char operator = sc.next().charAt(0);
 
-        System.out.println("operator = " + operator);
+        int result = 0;
 
+        if (operator == '+') {
+            result = num1 + num2;
+        } else if (operator == '-') {
+            result = num1 - num2;
+        } else if (operator == '*') {
+            result = num1 * num2;
+        } else if (operator == '/') {
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
+            }
+        } else {
+            System.out.println("기호를 잚못 입력했습니다.");
+        }
+
+        System.out.println("결과: " + result);
     }
 }
