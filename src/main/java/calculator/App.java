7e32efd38a8accbe8f585a3fee513df4790f1dc5
jiwoo <1114jiwoo@gmail.com>
2024-04-30 16:27:18 +0900

    Level1 - 2

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -12,6 +12,11 @@ public class App {
         System.out.print("두 번째 숫자를 입력하세요: ");
         int num2 = sc.nextInt();
 
-        System.out.println("num1, num2 = " + num1 + ", " + num2);
+        System.out.print("사칙연산 기호를 입력하세요: "); // +, -, *, /
+        /* charAt(idx) : charAt 메서드는 매개변수로 char 타입으로 변환하고자하는 문자열 위치 받는다*/
+        char operator = sc.next().charAt(0);
+
+        System.out.println("operator = " + operator);
+
     }
 }
